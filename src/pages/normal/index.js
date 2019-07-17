10ca354d492bdfaa1a140470fbe5bb80a10ba9f1
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-07-17 08:37:34 -0300

    add process button logic

--- src/pages/normal/index.js
+++ src/pages/normal/index.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import Visor from '../../components/Visor';
 import ButtonsContainer from './components/ButtonsContainer';
 import './styles.css';
+import processButton from './utils/process-button'
 
 export default class NormalPage extends Component {
   state = {
     value: ''
   }
 
-  onInput = (inputValue) => {
+  onInput = (buttonValue) => {
     const { value } = this.state
 
     this.setState({
-      value: `${value}${inputValue}`
+      value: processButton(value, buttonValue)
     })
   }
 
-  onEvaluate = () => {}
+  onEvaluate = () => {
+    alert('Not implemented yet')
+  }
 
   render () {
     return (
---
+++ src/pages/normal/utils/process-button.js
@@ -0,0 +1,59 @@
+import { isEmpty, includes, last, initial } from 'lodash-es'
+
+const actions = ['/', 'x', '-', '+']
+
+const separator = ' '
+
+const isAction = val => includes(actions, val)
+
+const concat = (stateValue, buttonValue, sep = separator) => {
+  return `${stateValue}${sep}${buttonValue}`
+}
+
+const isComma = val => val === ','
+
+const hasComma = str => str.includes(',')
+
+/**
+ * @method processButton
+ * @param  {String} buttonValue value from button
+ * @param  {String} stateValue  current value on state
+ * @return {String}
+ */
+const processButton = (stateValue, buttonValue) => {
+  if (isEmpty(stateValue)) {
+    if (isAction(buttonValue)) {
+      return ''
+    }
+
+    if (isComma(buttonValue)) {
+      return '0,'
+    }
+
+    return buttonValue
+  }
+
+  const stateValues = stateValue.split(separator)
+  const lastElement = last(stateValues)
+
+  if (isAction(lastElement) && isAction(buttonValue)) {
+    const initials = initial(stateValues).join(separator)
+    return concat(initials, buttonValue)
+  }
+
+  if (isAction(lastElement) && isComma(buttonValue)) {
+    return concat(stateValue, '0,')
+  }
+
+  if (!isAction(lastElement) && !isAction(buttonValue)) {
+    if (hasComma(lastElement) && isComma(buttonValue)) {
+      return concat(stateValue, '', '')
+    }
+
+    return concat(stateValue, buttonValue, '')
+  }
+
+  return concat(stateValue, buttonValue)
+}
+
+export default processButton
